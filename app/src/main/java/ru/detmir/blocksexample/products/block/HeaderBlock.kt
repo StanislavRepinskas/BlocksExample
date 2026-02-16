5753d6f7590e5d6dae45426edc0bc71cf8d8980d
stanislavrepinskas <repinskas@gmail.com>
2026-02-16 09:40:01 +0300

    +

--- app/src/main/java/ru/detmir/blocksexample/products/block/HeaderBlock.kt
+++ app/src/main/java/ru/detmir/blocksexample/products/block/HeaderBlock.kt
@@ -6,10 +6,18 @@ import ru.detmir.blocksexample.framework.block.Block
 class HeaderBlock @Inject constructor() : Block<HeaderBlock.State, Unit>() {
 
     override fun getInitialState(): State {
-        return State(text = "Products")
+        return State(
+            text = "Products",
+            filterState = null
+        )
     }
 
     data class State(
+        val text: String,
+        val filterState: FilterState?
+    )
+
+    data class FilterState(
         val text: String
     )
 }
